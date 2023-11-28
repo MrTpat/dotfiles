dfe46b7d325a8e66a0c7e83821ca2d39930cf618
Tilak Patel <ptilak142@gmail.com>
2023-11-28 00:46:42 -0500

    latest 2023

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,43 +3,36 @@ call plug#begin('~/.config/nvim')
 Plug 'morhetz/gruvbox'
 Plug 'junegunn/fzf'
 Plug 'junegunn/fzf.vim'
+Plug 'projekt0n/github-nvim-theme'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.4' }
 call plug#end()
 ]])
 
+
+vim.opt.number = true
+vim.opt.hidden = true
+vim.opt.cursorline = true
+vim.opt.smartindent = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.ai = true
+vim.opt.ignorecase = true
+vim.opt.swapfile = false
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = unnamed
+vim.opt.hlsearch = false
+vim.opt.laststatus = 0
+
 vim.cmd([[
-set hidden
-set cursorline
-set smartindent
-set incsearch
-set scrolloff=8
-set ai
-set ignorecase
-set noswapfile
-set number
-colorscheme gruvbox
-set clipboard=unnamed
-set softtabstop=4
-set shiftwidth=4
-set nohlsearch
+colorscheme github_dark_high_contrast
 ]])
 
-vim.env.FZF_DEFAULT_COMMAND = 'ag --hidden -g ""'
-
-vim.api.nvim_set_keymap(
-"n",
-"<c-p>",
-":Files<cr>",
-{ noremap = true }
-)
-
--- file CONTENT only search, no file name
-vim.cmd([[
-command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)
-]])
-
-vim.api.nvim_set_keymap(
-"n",
-"<c-g>",
-":Ag<cr>",
-{ noremap = true }
-)
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<c-p>', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<c-g>', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
